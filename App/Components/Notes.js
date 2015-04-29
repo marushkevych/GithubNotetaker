55b53c05815c6457e84c95b4db850a4dac92f1af
marushkevych <marushkevych@gmail.com>
2015-04-29 16:19:56 -0400

    resolving ListView issue

--- App/Components/Notes.js
+++ App/Components/Notes.js
@@ -12,92 +12,6 @@ var {
     TextInput
 } = React;
 
-
-class Notes extends React.Component{
-	constructor(props){
-		super(props);
-		var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-		this.state = {
-			 dataSource: ds.cloneWithRows(this.props.notes),
-			 note: '',
-			 error: ''
-		}
-
-	}
-	handleChange(e){
-		this.setState({
-			note: e.nativeEvent.text
-		})
-	}
-	handleSubmit(e){
-		var user = this.props.userInfo.login;
-		var note = this.state.note;
-		this.setState({
-			note: ''
-		});
-
-		api.addNote(user, note).then((res) => {
-			api.getNotes(user).then((notes) =>{
-				console.log('notes', notes)
-				this.setState({
-					dataSource: ds.cloneWithRows(['foo', 'bar'])
-				});
-			});
-		}).catch((error) => {
-			console.log('Request failed', error);
-			this.setState({error})
-		});
-	}
-	footer(){
-		return (
-			<View style={styles.footerContainer}>
-				<TextInput
-					style={styles.searchInput}
-					value={this.state.note}
-					onChange={this.handleChange.bind(this)}
-					placehplder="New Note" />
-				<TouchableHighlight 
-					style={styles.button}
-					onPress={this.handleSubmit.bind(this)}
-					underlayColor="#88D4F5">
-						<Text style={styles.buttonText}>Submit</Text>
-				</TouchableHighlight>
-			</View>
-		)
-	}
-	renderRow(rowData){
-		console.log('rowData', rowData)
-		return (
-			<View>
-				<View style={styles.rowContainer}>
-					<Text>{rowData}</Text>
-				</View>
-				<Separator/>
-			</View>
-		)
-	}
-    render(){
-        return (
-        	<View style={styles.container}>
-        		<ListView
-        			dataSource={this.state.dataSource}
-        			renderRow={this.renderRow}
-        			renderHeader={() => <Badge userInfo={this.props.userInfo} />} />
-            	
-            	{this.footer()}
-            </View>
-        );
-    }
-}
-
-module.exports = Notes;
-
-Notes.propTypes = {
-	userInfo: React.PropTypes.object.isRequired,
-	notes: React.PropTypes.object.isRequired
-}
-
-
 var styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -130,3 +44,91 @@ var styles = StyleSheet.create({
     flexDirection: 'row'
   }
 });
+
+
+class Notes extends React.Component{
+	constructor(props){
+		super(props);
+		var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			 dataSource: ds.cloneWithRows(this.props.notes),
+			 note: '',
+			 error: ''
+		}
+
+	}
+	handleChange(e){
+		this.setState({
+			note: e.nativeEvent.text
+		})
+	}
+	handleSubmit(e){
+		var user = this.props.userInfo.login;
+		var note = this.state.note;
+		this.setState({
+			note: ''
+		});
+
+		api.addNote(user, note).then((res) => {
+			api.getNotes(user).then((notes) =>{
+				console.log('notes', notes)
+				this.setState({
+					dataSource: ds.cloneWithRows(notes)
+				});
+			});
+		}).catch((error) => {
+			console.log('Request failed', error);
+			this.setState({error})
+		});
+	}
+	renderRow(rowData){
+		console.log('rowData', rowData)
+		return (
+			<View>
+				<View style={styles.rowContainer}>
+					<Text>{rowData}</Text>
+				</View>
+				<Separator/>
+			</View>
+		)
+	}
+  footer(){
+    return (
+      <View style={styles.footerContainer}>
+        <TextInput
+          style={styles.searchInput}
+          value={this.state.note}
+          onChange={this.handleChange.bind(this)}
+          placehplder="New Note" />
+        <TouchableHighlight 
+          style={styles.button}
+          onPress={this.handleSubmit.bind(this)}
+          underlayColor="#88D4F5">
+            <Text style={styles.buttonText}>Submit</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+  render(){
+      return (
+      	<View style={styles.container}>
+      		<ListView
+      			dataSource={this.state.dataSource}
+      			renderRow={this.renderRow}
+      			renderHeader={() => <Badge userInfo={this.props.userInfo} />} />
+          	
+        	{this.footer()}
+        </View>
+      );
+  }
+}
+
+
+Notes.propTypes = {
+	userInfo: React.PropTypes.object.isRequired,
+	notes: React.PropTypes.object.isRequired
+}
+
+module.exports = Notes;
+
+
